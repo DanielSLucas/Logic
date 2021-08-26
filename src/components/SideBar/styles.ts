ad8316d381abe69c7b545df134c8d20177e9dfb1
Daniel Lucas Santos <daniellucas-pms@hotmail.com>
2021-08-26 15:15:52 -0300

    styles: nodes styles

--- src/components/SideBar/styles.ts
+++ src/components/SideBar/styles.ts
@@ -5,4 +5,31 @@ export const Container = styled.aside`
   width: 20rem;
   background-color: #FAFAFC;
   box-shadow: 3px 0px 5px rgba(0, 0, 0, 0.25);
+
+  div.dndnode {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    padding: 1rem;
+    margin: 0.5rem;
+
+    border: 1px solid #d3d3d3;
+    border-radius: 0.5rem;
+
+    transition: box-shadow 0.2s;
+
+    svg {
+      margin-right: 1rem;
+      width: 2rem;
+      height: 100%;
+      stroke: #323232;
+    }
+
+    &:hover {
+      box-shadow: 3px 0px 5px rgba(0, 0, 0, 0.25);
+    }
+  }
+
+  
 `
