f76fae83aa88a5d08373e3e417149617401611af
Daniel Lucas Santos <daniellucas-pms@hotmail.com>
2021-08-31 15:17:51 -0300

    refactor: remove comment

--- src/components/LogicGatesBar/index.tsx
+++ src/components/LogicGatesBar/index.tsx
@@ -7,8 +7,7 @@ const LogicGatesBar: React.FC = () => {
   };
 
   return (
-    <Container>
-      {/* <div className="description">You can drag these nodes to the pane on the right.</div> */}
+    <Container>      
       <div className="dndnode and" onDragStart={(event) => onDragStart(event, 'and')} draggable>
         <svg xmlns="http://www.w3.org/2000/svg" width="32" height="27" viewBox="0 0 32 27" fill="none">
           <path d="M0.5 0.5H18.5C25.6797 0.5 31.5 6.3203 31.5 13.5C31.5 20.6797 25.6797 26.5 18.5 26.5H0.5V0.5Z" />
